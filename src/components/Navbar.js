75fa6ec4c72088e424d3c3678e68c81b9c2f722a
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2022-08-12 18:10:08 -0500

    Hero Banner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,8 +18,8 @@ const Navbar = () => {
                 fontSize="24px"
                 alignItems="flex-end"
             >
-                <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-                <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+                <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Inicio</Link>
+                <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Ejercicios</a>
             </Stack>
         </Stack>
     )
